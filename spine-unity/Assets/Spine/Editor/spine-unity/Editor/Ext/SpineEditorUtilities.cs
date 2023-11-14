1cf5c84943294a0dccf98a1133d6529a4cefa3da
xuss <977621265@qq.com>
2023-11-14 17:53:10 +0800

    资源导入路径过滤

---
+++ spine-unity/Assets/Spine/Editor/spine-unity/Editor/Ext/SpineEditorUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Spine.Unity.Editor
+{
+    public class SpineEditorUtilitiesExt
+    {
+        private static List<string> assetsCache = new List<string>();
+
+        public static string[] DoAssetsFilter(string[] imported)
+        {
+            assetsCache.Clear();
+            foreach (string asset in imported)
+            {
+                if (asset.Contains("Assets/AppearanceAssets"))
+                    assetsCache.Add(asset);
+            }
+
+            return assetsCache.ToArray();
+        }
+    }
+}
